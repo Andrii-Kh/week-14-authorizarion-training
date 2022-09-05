04d3014cbd3767915fb79b365d6fb394cf8551e0
Andrii <kharchenko.andrii.ua@gmail.com>
2022-09-05 17:30:41 +0300

    create component login and reducer auth, finish watched at 10:45 video autorization

---
+++ client/components/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from './head'
+import LoginForm from './login'
+
+// import wave from '../assets/images/wave.jpg'
+
+const Home = () => {
+
+  return (
+    <div>
+      <Head title="Home" />
+      <LoginForm />
+    </div>
+  )
+}
+
+Home.propTypes = {}
+
+export default Home
